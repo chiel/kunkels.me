85c369670114507c22bf51f200d13d9231ae7254
Chiel Kunkels <chiel@kunkels.me>
2017-12-19 00:39:28 +0100

    home: add slides thingy for titles

--- src/app/components/Intro.js
+++ src/app/components/Intro.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import Slides from './Slides';
+
 import css from '../styles/intro.css';
 
 export default function Intro() {
+	const titles = ['full-stack developer', 'devops enthusiast'];
+
 	return (
 		<section className={css.container}>
 			<h1>
 				Chiel <span>Kunkels</span>
 			</h1>
+			<Slides className={css.titles} loopInterval={4000}>
+				{titles.map(title => <p key={title}>{title}</p>)}
+			</Slides>
 		</section>
 	);
 }
---
+++ src/app/components/Slides.js
@@ -0,0 +1,75 @@
+import PT from 'prop-types';
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import css from '../styles/slides.css';
+
+const classNames = {
+	enter: css.slideIn,
+	enterActive: css.slideInActive,
+	exit: css.slideOut,
+	exitActive: css.slideOutActive,
+};
+
+export default class Slides extends React.PureComponent {
+	static propTypes = {
+		children: PT.node.isRequired,
+		className: PT.string,
+		loopInterval: PT.number,
+	};
+
+	static defaultProps = {
+		className: undefined,
+		loopInterval: undefined,
+	};
+
+	constructor() {
+		super();
+
+		this.state = { active: 0 };
+	}
+
+	componentDidMount() {
+		if (this.props.loopInterval) {
+			this.queue();
+		}
+	}
+
+	goto(index) {
+		if (index > this.props.children.length - 1) {
+			index = 0;
+		}
+
+		if (index < 0) {
+			index = this.props.children.length - 1;
+		}
+
+		this.setState({ active: index });
+	}
+
+	next() {
+		this.goto(this.state.active + 1);
+	}
+
+	queue() {
+		setTimeout(() => {
+			this.next();
+			this.queue();
+		}, this.props.loopInterval);
+	}
+
+	render() {
+		const { children, className } = this.props;
+		const { active } = this.state;
+
+		const item = children[active];
+
+		return (
+			<TransitionGroup className={className} component="div">
+				<CSSTransition key={item.key} classNames={classNames} timeout={2000}>
+					{item}
+				</CSSTransition>
+			</TransitionGroup>
+		);
+	}
+}
